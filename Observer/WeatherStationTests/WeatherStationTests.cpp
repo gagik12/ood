84ca2e0fb5881c362308ed3b6eedd6c793e171fd
vadimDol <vadim.dolganof@gmail.com>
2018-02-16 03:05:47 +0300

    Добавил тесты.

---
+++ Observer/WeatherStationTests/WeatherStationTests.cpp
@@ -0,0 +1,117 @@
+#include "stdafx.h"
+#include "../WeatherStation/Observer.h"
+#include "../WeatherStation/WeatherData.h"
+
+using namespace std;
+
+namespace TestObserverPriority
+{
+	class CTestDisplay : public IObserver<SWeatherInfo>
+	{
+	public:
+		CTestDisplay(boost::test_tools::output_test_stream & stream)
+			: m_stream(stream)
+		{}
+	private:
+		void Update(SWeatherInfo const& data) override
+		{
+			m_stream << "CTestDisplay\n";
+		}
+		boost::test_tools::output_test_stream & m_stream;
+	};
+
+	class CTestStatsDisplay : public IObserver<SWeatherInfo>
+	{
+	public:
+		CTestStatsDisplay(boost::test_tools::output_test_stream & stream)
+			: m_stream(stream)
+		{}
+	private:
+		void Update(SWeatherInfo const& data) override
+		{
+			m_stream << "CTestStatsDisplay\n";
+		}
+		boost::test_tools::output_test_stream & m_stream;
+	};
+}
+
+BOOST_AUTO_TEST_SUITE(task_3)
+	BOOST_AUTO_TEST_CASE(can_set_the_priority)
+	{
+		boost::test_tools::output_test_stream stream;
+		
+		CWeatherData wd;
+
+		TestObserverPriority::CTestDisplay testDisplay(stream);
+		TestObserverPriority::CTestStatsDisplay testStatsDisplay(stream);
+
+		wd.RegisterObserver(testDisplay, 100);
+		wd.RegisterObserver(testStatsDisplay, 10);
+		wd.SetMeasurements(3, 0.7, 760);
+		BOOST_CHECK(stream.is_equal("CTestDisplay\nCTestStatsDisplay\n"));
+
+		stream.clear();
+		wd.RemoveObserver(testDisplay);
+		wd.RemoveObserver(testStatsDisplay);
+
+		wd.RegisterObserver(testDisplay, 10);
+		wd.RegisterObserver(testStatsDisplay, 100);
+		wd.SetMeasurements(3, 0.7, 760);
+		BOOST_CHECK(stream.is_equal("CTestStatsDisplay\nCTestDisplay\n"));
+	}
+BOOST_AUTO_TEST_SUITE_END()
+
+namespace TestSafeNotificationObservers
+{
+	class CTestDisplay : public IObserver<SWeatherInfo>
+	{
+	public:
+		CTestDisplay(boost::test_tools::output_test_stream & stream, CObservable<SWeatherInfo> & Observable)
+			: m_stream(stream)
+			, m_Observable(Observable)
+		{}
+	private:
+		void Update(SWeatherInfo const& data) override
+		{
+			m_Observable.RemoveObserver(*this);
+			m_stream << "Removed CTestDisplay\n";
+		}
+
+		boost::test_tools::output_test_stream & m_stream;
+		CObservable<SWeatherInfo> & m_Observable;
+	};
+
+	class CTestStatsDisplay : public IObserver<SWeatherInfo>
+	{
+	public:
+		CTestStatsDisplay(boost::test_tools::output_test_stream & stream, CObservable<SWeatherInfo> & Observable)
+			: m_stream(stream)
+			, m_Observable(Observable)
+		{}
+	private:
+		void Update(SWeatherInfo const& data) override
+		{
+			m_Observable.RemoveObserver(*this);
+			m_stream << "Removed CTestStatsDisplay\n";
+		}
+		boost::test_tools::output_test_stream & m_stream;
+		CObservable<SWeatherInfo> & m_Observable;
+	};
+}
+
+BOOST_AUTO_TEST_SUITE(task_2)
+	BOOST_AUTO_TEST_CASE(safe_notification_observers)
+	{
+		boost::test_tools::output_test_stream stream;
+
+		CWeatherData wd;
+
+		TestSafeNotificationObservers::CTestDisplay testDisplay(stream, wd);
+		TestSafeNotificationObservers::CTestStatsDisplay testStatsDisplay(stream, wd);
+
+		wd.RegisterObserver(testDisplay, 2);
+		wd.RegisterObserver(testStatsDisplay, 1);
+		wd.SetMeasurements(3, 0.7, 760);
+		BOOST_CHECK(stream.is_equal("Removed CTestDisplay\nRemoved CTestStatsDisplay\n"));
+	}
+BOOST_AUTO_TEST_SUITE_END()
---
+++ Observer/WeatherStationTests/stdafx.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+
+#define BOOST_TEST_INCLUDED
+#pragma warning (disable: 4702)
+#include <boost/test/unit_test.hpp>
+#include <boost/lexical_cast.hpp>
+#include <functional>
+#include <climits>
+#include <boost/test/output_test_stream.hpp>
