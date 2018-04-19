eeb43e3b5a39ea975fd7b0c27fa2217f8443f9d4
vadimDol <vadim.dolganof@gmail.com>
2018-04-20 01:49:18 +0300

    Доработал тесты дизайнера

--- Factory/Painter/PainterTest/DesignerTests.cpp
+++ Factory/Painter/PainterTest/DesignerTests.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "../Designer.h"
+#include "../IDesigner.h"
 #include "../Shape.h"
 #include "../PictureDraft.h"
 #include "../Triangle.h"
@@ -7,19 +8,52 @@
 
 typedef std::vector<std::string> ShapeDescriptions;
 
+class Designer : public IDesigner
+{
+public:
+	Designer(IShapeFactory& factory, boost::test_tools::output_test_stream & testStream)
+		: m_factory(factory)
+		, m_testStream(testStream)
+	{}
+
+	CPictureDraft CreateDraft(std::istream &data)
+	{
+		CPictureDraft draft;
+		std::string line;
+		while (std::getline(data, line))
+		{
+			try
+			{
+				auto shape = m_factory.CreateShape(line);
+				draft.AddShape(std::move(shape));
+				m_testStream << line << std::endl;
+			}
+			catch (std::exception & e)
+			{
+				std::cout << e.what() << std::endl;
+			}
+		}
+		return draft;
+	}
+private:
+	IShapeFactory& m_factory;
+	boost::test_tools::output_test_stream & m_testStream;
+};
+
 struct SDesignerFixture
 {
 	SDesignerFixture()
 		: factory(CShapeFactory())
-		, designer(factory)
+		, designer(factory, testStream)
 	{
 	}
 
 	CShapeFactory factory;
-	CDesigner designer;
+	Designer designer;
+	boost::test_tools::output_test_stream testStream;
 };
 
-BOOST_FIXTURE_TEST_SUITE(Designer, SDesignerFixture)
+BOOST_FIXTURE_TEST_SUITE(SDesigner, SDesignerFixture)
 	BOOST_AUTO_TEST_CASE(returns_empty_draft_when_stream_is_empty)
 	{
 		std::istringstream stream;
@@ -30,7 +64,8 @@ BOOST_FIXTURE_TEST_SUITE(Designer, SDesignerFixture)
 	BOOST_AUTO_TEST_CASE(can_create_draft)
 	{
 		ShapeDescriptions shapeDescriptions = {
-			"Trinagle red 10 10 200 10 100 50",
+			"Triangle red 10 10 200 10 100 50",
+			"Ellipse blue 100 100 200 10",
 			"Rectangle green 10 10 200 10"
 		};
 
@@ -42,5 +77,9 @@ BOOST_FIXTURE_TEST_SUITE(Designer, SDesignerFixture)
 
 		auto draft = designer.CreateDraft(stream);
 		BOOST_CHECK(!draft.IsEmpty());
+		BOOST_CHECK(testStream.is_equal(
+			"Triangle red 10 10 200 10 100 50\n"
+			"Ellipse blue 100 100 200 10\n"
+			"Rectangle green 10 10 200 10\n"));
 	}
 BOOST_AUTO_TEST_SUITE_END()
